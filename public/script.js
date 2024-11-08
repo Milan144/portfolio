4ba754bd57c4c17ecd74199ae4cbf158befec854
Milan144 <milan.hommet@protonmail.com>
2024-11-08 10:24:07 +0100

    add education and experiences

--- public/script.js
+++ public/script.js
@@ -1,23 +1,4 @@
 document.addEventListener('DOMContentLoaded', async() => {
-  const modal = document.getElementById('cv-modal');
-  const modalImg = document.getElementById('modal-img');
-  const cvImage = document.getElementById('cv-image');
-  const closeBtn = document.getElementsByClassName('close')[0];
-
-  cvImage.onclick = function(event) {
-    event.preventDefault();
-    modal.style.display = 'block';
-    modalImg.src = this.src;
-  }
-
-  closeBtn.onclick = function() {
-    modal.style.display = 'none';
-  }
-
-  modal.onclick = function() {
-    modal.style.display = 'none';
-  }
-
   const username = "milan144";
   const repoCount = 10;
   const apiUrl = `https://api.github.com/users/${username}/repos?per_page=${repoCount}&sort=updated`;
